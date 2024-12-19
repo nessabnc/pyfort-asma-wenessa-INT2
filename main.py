88ec6940206b2c402cd00612c9e9503223b98d2a
nessabnc <wenessa.benchaiba@efrei.net>
2024-12-19 16:56:20 +0100

    yesss

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from math_challenges import *
+math_challenge_equation()
+
+
+
+
 #Projetfortboyard
 
--- math_challenges.py
+++ math_challenges.py
@@ -1,10 +1,12 @@
+import random
+
 def factorial(n):
     fac= 1
     for i in range(1,n+1):
         fac= fac*i
     return fac
 
-import random
+
 def math_challenge_factorial():
     n=0
     n= random.randint(1,10)
@@ -13,8 +15,10 @@ def math_challenge_factorial():
     num= int(input("Your answer: \n"))
     if num == ans:
         print("Correct! You win a key")
+        return True
     else:
         print("Incorrect, better luck next time...")
+        return False
 
 
 
@@ -25,5 +29,74 @@ def solve_linear_equation():
     return a,b, x
 
 def math_challenge_equation():
-    solve_linear_equation()
-    print()
+    a, b, x = solve_linear_equation()
+    print("What is", a, "x+", b)
+    r= f"{-b}/{a}"
+    z=str(int(-b/a))
+    t= input("Enter your answer: \n")
+    if t==r or t==z:
+        print("Correct! You win a key")
+        return True
+    else :
+        print("Incorrect")
+        return False
+
+
+def is_prime(n):
+    if n<2:
+        return False
+    if n==2:
+        return True
+    if n%2==0:
+        return False
+    else :
+        i = 3
+        while i*i<=n:
+            if n%i==0:
+                return False
+            else :
+                i +=2
+        return True
+
+def nearest_prime(n):
+    while not is_prime(n):
+        n +=2
+    return n
+
+
+def math_challenge_prime():
+    a= random.randint(10,20)
+    answer= int(input("Enter the prime number closest to" + ' '+ str(a)))
+    n = nearest_prime(answer)
+    if answer == n:
+        print("Correct! You win a key")
+        return True
+    else :
+        print("Incorrect")
+        return False
+
+def math_challenge():
+    challenges= [math_challenge_prime, math_challenge_factorial]
+    challenge= random.choice(challenges)
+    print(challenge())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
