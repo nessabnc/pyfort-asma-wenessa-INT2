b941fa67e5279442afa34251224e9c4c05760278
nessabnc <wenessa.benchaiba@efrei.net>
2024-12-31 10:22:29 +0100

    yesss

--- utility_functions.py
+++ utility_functions.py
@@ -1,4 +1,6 @@
-
+def introduction():
+    print("Welcome to Fort Boyard Simulator\n ", "In this game you will need to complete challenges in order to obtain as many keys as possible to unlock a treasure room.\n",
+          "If you manage to succeed in a challenge, you will a key. However, if you fail the challenge, you don't win anything\n", "In order to unlock the treasure room, you must collect three keys.\n", "If you fail at collecting the three keys, you fail at the game. Good luck ;)" )
 
 
 
